25773bcfc1e42e98bd6c55b12da742abd08e8762
june <belencastillocha@gmail.com>
2025-05-19 09:43:46 +0200

    wdq

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Layout from './layout/Layout.jsx';
 import Home from './pages/Home.jsx';
 import AvisoLegal from './pages/AvisoLegal.jsx';
@@ -6,14 +6,11 @@ import Productos from './pages/Productos.jsx';
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Layout><Home /></Layout>} />
-        <Route path="/aviso-legal" element={<Layout><AvisoLegal /></Layout>} />
-        <Route path="/productos" element={<Layout><Productos /></Layout>} />
-
-      </Routes>
-    </Router>
+    <Routes>
+      <Route path="/" element={<Layout><Home /></Layout>} />
+      <Route path="/productos" element={<Layout><Productos /></Layout>} />
+      <Route path="/aviso-legal" element={<Layout><AvisoLegal /></Layout>} />
+    </Routes>
   );
 }
 
